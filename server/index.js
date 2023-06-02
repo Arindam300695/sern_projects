fd7930100c1fd567b9887fdbcfa8b3f6f8fced5f
Arindam300695 <114329016+Arindam300695@users.noreply.github.com>
2023-06-02 11:09:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const connection = async () => {
 			app.listen(8080, (err) => {
 				if (!err)
 					console.log(
-						"app is listening on https://online-book-store-iktx.onrender.com",
+						"app is listening on http://localhost:8080",
 					);
 				else console.log(err);
 			});
